2ceb5af200b3984107550bb165d87cf4f2a18121
Alina Kitaeva <akitaeva@gmail.com>
2018-07-12 21:03:56 -0400

    done

--- starter_code/public/javascript/APIHandler.js
+++ starter_code/public/javascript/APIHandler.js
@@ -1,25 +1,67 @@
-class APIHandler {
-  constructor (baseUrl) {
-    this.BASE_URL = baseUrl;
-  }
+const baseurl = "https://ih-crud-api.herokuapp.com";
 
-  getFullList () {
 
-  }
-
-  getOneRegister () {
-
-  }
-
-  createOneRegister () {
-
-  }
-
-  updateOneRegister () {
-
-  }
-
-  deleteOneRegister () {
-
-  }
+ function getFullList () {
+   axios.get(baseurl + '/characters')
+   .then(res => {
+    $('.characters-container').empty();
+    res.data.forEach((oneCharacter) => {
+        // console.log("oneCharacter: ", oneCharacter)
+        const newChar = 
+        `
+        <div>
+        <div>Name: ${oneCharacter.name}</div>
+        <div>Occupation: ${oneCharacter.occupation}</div>
+        <div>Debt: ${oneCharacter.debt}</div>
+        <div>Weapon: ${oneCharacter.weapon}</div>
+        <div>ID:${oneCharacter.id}  </div>
+        </div>
+        <hr>
+        `
+        $('.characters-container').append(newChar);
+      });
+    })
+   .catch(err => console.log(err));
 }
+
+
+  function getOneRegister (id) {
+    axios.get(baseurl + '/characters/' + id)
+    .then(res => {
+    $('.characters-container').empty();
+    const newChar = 
+    `
+    <div>
+    <div>Name: ${res.data.name}</div>
+    <div>Occupation: ${res.data.occupation}</div>
+    <div>Debt: ${res.data.debt}</div>
+    <div>Weapon: ${res.data.weapon}</div>
+    </div>
+    <hr>
+    `
+    $('.characters-container').append(newChar);
+
+    })
+    .catch(err => console.log(err));
+
+  }
+
+ function createOneRegister (data) {
+   axios.post(baseurl + '/characters/', data)
+   .then(res => console.log("the Character has been added:", res))
+   .catch(err => console.log(err));
+  }
+
+  function updateOneRegister (id, data) {
+    axios.patch(baseurl + '/characters/' + id, data)
+    .then(res => console.log(id, ' the element updated', res))
+    .catch(err => console.log(err)); 
+  }
+
+ function deleteOneRegister (id) {
+   axios.delete(baseurl + '/characters/' + id)
+   .then(res => console.log("Deleting complete", res))
+   .catch(err => console.log(err));
+
+  }
+
--- starter_code/public/javascript/index.js
+++ starter_code/public/javascript/index.js
@@ -1,23 +1,77 @@
-const charactersAPI = new APIHandler("http://localhost:8000")
+
 
 $(document).ready( () => {
-  document.getElementById('fetch-all').onclick = function(){
-
+    document.getElementById('fetch-all').onclick = function(){
+    getFullList()
   }
-  
+
+
   document.getElementById('fetch-one').onclick = function(){
     
+    const id = document.getElementsByName('character-id')[0].value;
+    getOneRegister(id);
   }
+
+  document.getElementById('new-character-form').onsubmit = function() {
+    event.preventDefault(); 
+
+    const name = document.getElementsByName('name')[0].value;
+    const occupation = document.getElementsByName('occupation')[0].value;
+    const weapon = document.getElementsByName('weapon')[0].value;
+
+   if (name ==='') {
+     alert('Fill in the "name" field');
+   } else if (occupation ==='') {
+    alert('Fill in the "occupation" field');
+   } else if (weapon ===''){
+    alert('Fill in the "weapon" field');
+   } else {
+     const newCharacter = {
+       name: document.getElementsByName('name')[0].value,
+       occupation: document.getElementsByName('occupation')[0].value,
+       debt: document.getElementsByName('cartoon')[0].checked,
+       weapon: document.getElementsByName('weapon')[0].value
+      }
+    console.log("New Character: ", newCharacter);
+    createOneRegister(newCharacter);
+   }
+
+  }
+
+document.getElementById('edit-character-form').onclick = function(){
+  event.preventDefault(); 
   
-  document.getElementById('delete-one').onclick = function(){
-        
-  }
+
+  const id = document.getElementsByName('chr-id')[0].value;
+  console.log(id);
+  const name = document.getElementsByName('name')[1].value;
+  const occupation = document.getElementsByName('occupation')[1].value;
+  const debtCartoon = document.getElementsByName('cartoon')[1].checked;
+  const weapon = document.getElementsByName('weapon')[1].value;
+
   
-  document.getElementById('edit-character-form').onsubmit = function(){
-            
-  }
-  
-  document.getElementById('new-character-form').onsubmit = function(){
-                
-  }
-})
+
+  const updCharacter = {};
+ 
+  if (name !=='') { updCharacter.name = name}
+  if (occupation !=='') { updCharacter.occupation = occupation}
+  if (debtCartoon === true) { updCharacter.debt = debtCartoon}
+  if (weapon !=='') { updCharacter.weapon = weapon}
+
+
+  updateOneRegister (id, updCharacter);
+  console.log(updCharacter) 
+
+}
+
+
+document.getElementById('delete-one').onclick = function(){
+    const id = document.getElementsByName("character-id-delete")[0].value;
+    console.log("The charcter with ", id, " has been deleted");
+    deleteOneRegister(id);
+  };
+
+
+
+});
+
